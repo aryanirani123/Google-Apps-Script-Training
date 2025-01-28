d44e9cb120a74cfab6a2e737b37c434c5e1dab10
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2025-01-28 11:24:30 +0530

    Update Loops.js

--- Loops.js
+++ Loops.js
@@ -1,6 +1,63 @@
-function demoScript() {
-  let numbers = [1, 2, 3, 4, 5];
-  numbers.forEach(function(num) {
-    Logger.log(num * 2); // Log doubled values
-  });
-}
+function loops(){
+
+
+    const namesarray = ['Aryan','Neil','Raj'];
+    Logger.log(namesarray)
+  /*
+    Logger.log(namesarray[0])
+    Logger.log(namesarray[1])
+    Logger.log(namesarray[2])
+  */
+  
+    for(let i = 0;i<namesarray.length;i++){
+  
+      Logger.log(namesarray[i])
+  
+  
+    }
+  
+  
+  }
+  
+  function forinloop(){
+  
+      const car = {
+  
+      "name" : "Tesla",
+      "model number" : 3,
+      "Colour" : "Green",
+      "YOB" : 2020,
+    }
+  
+  
+    for(const details in car){
+  
+      Logger.log(details)
+  
+      Logger.log(car[details])
+  
+  
+    }
+  
+  }
+  
+  function ForEachLoop(){
+  
+    let sum = 0;
+  
+    const numbers = [65,44,12,4];
+  
+    numbers.forEach(myfunction);
+  
+    Logger.log(sum)
+  
+    function myfunction(item){
+  
+      sum +=item
+  
+    }
+  
+  }
+
+
+  
